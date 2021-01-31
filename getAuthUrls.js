ca861d2d2f426175fe7c74b3cc9dff4c160e56ff
ZoBro23 <zohan.subhash@gmail.com>
2021-01-31 10:26:27 +0000

    Beautified

--- getAuthUrls.js
+++ getAuthUrls.js
@@ -5,28 +5,40 @@ const github = 'https://github.com/login/oauth/authorize'
 const google = 'https://accounts.google.com/o/oauth2/v2/auth'
 const microsoft = 'https://login.microsoftonline.com/common/oauth2/v2.0/authorize'
 const axios = require('axios')
-const {cred} = require('./setCredentials')
+const {
+    cred
+} = require('./setCredentials')
 const querystring = require('query-string')
 
-const urls = {apple: apple, discord: discord, facebook: facebook, github: github, google: google, microsoft: microsoft}
-
-function getAuthUrls(company, options){
-for(key in cred){
-  if(Array.isArray(cred[key].scope)){
-    cred[key].scope = cred[key].scope.join(' ')
-  }
+const urls = {
+    apple: apple,
+    discord: discord,
+    facebook: facebook,
+    github: github,
+    google: google,
+    microsoft: microsoft
 }
- const params = {
-client_id: cred[company].client_id,
-cllient_secret: cred[company].client_secret,
-redirect_uri: cred[company].redirect_uri,
-scope: cred[company].scope,
-response_type: cred[company].response_type,
- }
+
+function getAuthUrls(company, options) {
+    for (key in cred) {
+        if (Array.isArray(cred[key].scope)) {
+            cred[key].scope = cred[key].scope.join(' ')
+        }
+    }
+    const params = {
+        client_id: cred[company].client_id,
+        cllient_secret: cred[company].client_secret,
+        redirect_uri: cred[company].redirect_uri,
+        scope: cred[company].scope,
+        response_type: cred[company].response_type,
+    }
 
 
-const final = {...params, ...options}
-return `${urls[company]}?${querystring.stringify(final)}`
+    const final = {
+        ...params,
+        ...options
+    }
+    return `${urls[company]}?${querystring.stringify(final)}`
 }
 
 module.exports = getAuthUrls
--- setCredentials.js
+++ setCredentials.js
@@ -1,18 +1,23 @@
-let cred = {google: {
-client_id: '916671016794-3pd46ugeibu22bjgg5o33uq19cdfo4qa.apps.googleusercontent.com', 
-client_secret: 'yD-6UADTjc63T6Owp6y_PTPv', 
-redirect_uri: 'https://google.com', 
-scope: ['profile', 'email'], 
-response_type: 'code'
-}}
-
-function setCredentials(credentials){
-for(key in credentials){
-  cred[key] = credentials[key]
-}
-for(key in cred){
-  cred[key].scope = cred[key].scope.join(' ')
-}
+let cred = {
+    google: {
+        client_id: '916671016794-3pd46ugeibu22bjgg5o33uq19cdfo4qa.apps.googleusercontent.com',
+        client_secret: 'yD-6UADTjc63T6Owp6y_PTPv',
+        redirect_uri: 'https://google.com',
+        scope: ['profile', 'email'],
+        response_type: 'code'
+    }
 }
 
-module.exports = {cred, setCredentials}  
+function setCredentials(credentials) {
+    for (key in credentials) {
+        cred[key] = credentials[key]
+    }
+    for (key in cred) {
+        cred[key].scope = cred[key].scope.join(' ')
+    }
+}
+
+module.exports = {
+    cred,
+    setCredentials
+}
