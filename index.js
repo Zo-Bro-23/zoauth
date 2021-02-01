05828094ae902559e1d1b1b728c51521773a506f
Guest User <Guest@Subhashs-MacBook-Pro.local>
2021-02-01 16:55:20 +0530

    switch to repl

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 const getAuthUrls = require('./getAuthUrls')
 const express = require('express')
 const app = express()
-console.log(getAuthUrls('github'))
+// console.log(getAuthUrls('github'))
+// app.get('/', (req, resp) => {resp.sendFile('./index.html')})
+// app.listen(5210)
+const data = '{ "name": "Flavio", "age": 35 }'
+try {
+  const user = JSON.parse(data)
+} catch(err) {
+  console.error(err)
+}
--- setCredentials.js
+++ setCredentials.js
@@ -1,48 +1,5 @@
 const dotenv = require('dotenv').config()
-let cred = {
-    google: {
-        client_id: process.env.google_client_id,
-        client_secret: process.env.google_client_secret,
-        redirect_uri: 'https://google.com',
-        scope: ['profile', 'email'],
-        response_type: 'code'
-    },
-    microsoft: {
-        client_id: process.env.microsoft_client_id,
-        client_secret: process.env.microsoft_client_secret,
-        redirect_uri: 'https://google.com',
-        scope: ['openid', 'email'],
-        response_type: 'code'
-    },
-    discord: {
-        client_id: process.env.discord_client_id,
-        client_secret: process.env.discord_client_secret,
-        redirect_uri: 'https://google.com',
-        scope: ['email'],
-        response_type: 'code'
-    },
-    facebook: {
-        client_id: process.env.facebook_client_id,
-        client_secret: process.env.facebook_client_secret,
-        redirect_uri: 'https://google.com',
-        scope: ['email'],
-        response_type: 'code'
-    },
-    github: {
-        client_id: process.env.github_client_id,
-        client_secret: process.env.github_client_secret,
-        redirect_uri: 'https://google.com',
-        scope: ['email'],
-        response_type: 'code'
-    },
-    amazon: {
-        client_id: process.env.amazon_client_id,
-        client_secret: process.env.amazon_client_secret,
-        redirect_uri: 'https://google.com',
-        scope: ['profile'],
-        response_type: 'code'
-    }
-}
+let cred = JSON.parse(process.env.cred)
 
 function setCredentials(credentials) {
     for (key in credentials) {
