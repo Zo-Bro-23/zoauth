c61c78a96ee64daaa6c8a9c841be8a9f4ee7a1b9
Guest User <Guest@Subhashs-MacBook-Pro.local>
2021-02-01 17:25:06 +0530

    Final 1/2/2021

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
 const getAuthUrls = require('./getAuthUrls')
 const express = require('express')
 const app = express()
-// console.log(getAuthUrls('github'))
-// app.get('/', (req, resp) => {resp.sendFile('./index.html')})
-// app.listen(5210)
-const data = '{ "name": "Flavio", "age": 35 }'
-try {
-  const user = JSON.parse(data)
-} catch(err) {
-  console.error(err)
-}
+console.log(getAuthUrls('github'))
+app.get('/', (req, resp) => {resp.sendFile('index.html', {root: '.'})})
+app.listen(5210)
